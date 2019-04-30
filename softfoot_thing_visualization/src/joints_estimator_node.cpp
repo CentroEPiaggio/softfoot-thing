9271db8c1766e6c2023421d527749058cdc1865f
Mathew Jose Pollayil <mathewjose.pollayil@phd.unipi.it>
2019-04-30 17:37:27 +0200

    Single launch for visualization and code clean up

--- softfoot_thing_visualization/src/joints_estimator_node.cpp
+++ softfoot_thing_visualization/src/joints_estimator_node.cpp
@@ -22,28 +22,31 @@ int main(int argc, char** argv) {
     // Joint estimator objects
     softfoot_thing_visualization::JointsEstimator joint_estimator(nh, 3, "softfoot");
 
-    ROS_INFO_STREAM("SoftFoot Joint Estimator : started.");
+    ROS_INFO_STREAM("SoftFoot Joint Estimator : starting...");
 
     // If needed, calibrating after waiting for some time
-    sleep(2);
-
     if (online_calib) {
-        ROS_INFO_STREAM("SoftFoot Joint Estimator : starting to calibrate the sensing.");
+        ROS_INFO_STREAM("SoftFoot Joint Estimator : online calibration is set.");
+        ROS_INFO_STREAM("SoftFoot Joint Estimator : Please assure that the feet are on a flat" 
+            << " surface and don't move them until calibration is finished!" 
+            << "This might take some time...");
+        sleep(5);
+        ROS_INFO_STREAM("SoftFoot Joint Estimator : starting to calibrate the sensing...");
         joint_estimator.calibrate();
         ROS_INFO_STREAM("SoftFoot Joint Estimator : calibration finished.");
     }
 
     // Check if calibrated before spinning
-    ROS_INFO_STREAM("SoftFoot Joint Estimator : checking for correct calibration.");
+    ROS_INFO_STREAM("SoftFoot Joint Estimator : checking for correct calibration...");
     bool all_calibrated = true;
     if (!joint_estimator.check_calibration()) all_calibrated = false;
     if (!all_calibrated) {
-        ROS_INFO_STREAM("SoftFoot Joint Estimator : not all feet are calibrated...");
+        ROS_INFO_STREAM("SoftFoot Joint Estimator : not all feet are calibrated, this is bad!");
         return 1;
     }
 
     // Spin estimators as fast as possible until node is shut down
-    ROS_INFO_STREAM("SoftFoot Joint Estimator : starting to spin");
+    ROS_INFO_STREAM("SoftFoot Joint Estimator : starting to spin and estimate!");
     spinner.start();
     while (ros::ok()) {
 
